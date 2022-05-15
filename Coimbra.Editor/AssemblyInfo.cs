0e345d604ac75cf42373af25c8912a70df3131d2
Coimbra <coimbra.dev@outlook.com>
2022-05-15 02:36:22 -0300

    Fix missing minimum release (30f1) and missing UniTask dependency

--- Coimbra.Editor/AssemblyInfo.cs
+++ Coimbra.Editor/AssemblyInfo.cs
@@ -2,4 +2,3 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Coimbra.Editor.Tests")]
 [assembly: InternalsVisibleTo("Coimbra.Tests.Editor")]
-[assembly: InternalsVisibleTo("Coimbra.ExtendedInspector.Editor")]
