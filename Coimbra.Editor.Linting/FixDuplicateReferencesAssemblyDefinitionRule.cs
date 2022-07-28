5a19f02402e239b902ecd4c14784b834396333da
Coimbra <coimbra.dev@outlook.com>
2022-07-27 22:05:34 -0300

    Add FixDuplicateReferences ADR
    (cherry picked from commit c7211229ebf000ed03ec6d2f2098f1ed0ddac4b7)

---
+++ Coimbra.Editor.Linting/FixDuplicateReferencesAssemblyDefinitionRule.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coimbra.Editor.Linting
+{
+    /// <summary>
+    /// Fix assemblies containing duplicate references.
+    /// </summary>
+    [CreateAssetMenu(menuName = CoimbraUtility.GeneralMenuPath + DefaultAssetMenuPath + "Fix Duplicate References")]
+    public sealed class FixDuplicateReferencesAssemblyDefinitionRule : AssemblyDefinitionRuleBase
+    {
+        /// <inheritdoc/>
+        public override bool Apply(AssemblyDefinition assemblyDefinition, Object context)
+        {
+            using (ListPool.Pop(out List<string> referencesList))
+            using (HashSetPool.Pop(out HashSet<string> referencesSet))
+            {
+                string[] references = assemblyDefinition.References;
+                referencesList.EnsureCapacity(references.Length);
+
+                for (int i = 0; i < references.Length; i++)
+                {
+                    if (referencesSet.Add(references[i]))
+                    {
+                        referencesList.Add(references[i]);
+
+                        continue;
+                    }
+
+                    Debug.LogWarning($"{assemblyDefinition.Name} had duplicate reference to {references[i]}!", context);
+                }
+
+                if (referencesList.Count == references.Length)
+                {
+                    return false;
+                }
+
+                assemblyDefinition.References = referencesList.ToArray();
+
+                return true;
+            }
+        }
+    }
+}
